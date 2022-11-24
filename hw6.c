cbeca9abe609b28ae1f5c315b7b0d4ebe11fc3dd
YuC13600 <uivd978985@gmail.com>
2022-11-24 23:14:56 +0800

    hw6 modify

--- hw6.c
+++ hw6.c
@@ -7,7 +7,7 @@ int g(int x, int y){
 int main(void){
     for(;;){
         int N, sum=0;
-        scanf("%lld", &N);
+        scanf("%d", &N);
         if(N==0) return 0;
         int x, y;
         for(x=1;x<N;++x){
