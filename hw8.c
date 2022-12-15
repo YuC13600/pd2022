60dbf73aeb23f0299a9ed5dfe467944025f14f15
YuC13600 <uivd978985@gmail.com>
2022-12-15 18:24:01 +0800

    hw8

---
+++ hw8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int in, sum=0;
+    for(;;){
+        scanf("%d", &in);
+        if(!in) return 0;
+        while(in>=10){
+            sum=0;
+            do{
+                sum+=in%10;
+            }while(in/=10);
+            in=sum;
+        }
+        printf("%d\n", in);
+    }
+}
