7616dfcb217549e6ab924ea9a918d045dea7f0ce
YuC13600 <uivd978985@gmail.com>
2023-01-22 16:52:01 +0800

    hw1 : opti

--- HW1_release.c
+++ HW1_release.c
@@ -7,13 +7,9 @@ int main(void){
         int a, b, c, sum = 0;
         scanf("%d %d %d", &a, &b, &c);
         a += b;
-        while(1){
-            if(a/c != 0){
-                sum += a/c;
-                a = a%c + a/c;
-            }
-            else
-                break;
+        while(a/c != 0){
+            sum += a/c;
+            a = a%c + a/c;
         }
         printf("%d\n", sum);
     }
